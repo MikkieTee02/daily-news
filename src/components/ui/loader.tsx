1c79ae6c855361e22a1945cd449b9c95ae9fe220
MikkieTee02 <mikaelakayz@gmail.com>
2025-09-15 15:31:48 +0200

    add new component

--- src/components/ui/loader.tsx
+++ src/components/ui/loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
 	return (
 		<div className='w-full pt-4 flex justify-center'>
-			<span className='loading loading-dots loading-ls text-primary'></span>
+			<span className='loading loading-dots loading-ls text-purple-700'></span>
 		</div>
 	);
 }
